80e9e476246cc7b32c29cac929d95d81076d0b5b
Sunita-19 <sunitayadav190105@gmail.com>
2024-11-02 20:47:10 +0530

    Start pose working

--- frontend/src/components/DropDown/DropDown.js
+++ frontend/src/components/DropDown/DropDown.js
@@ -1,41 +1,39 @@
-import React from 'react'
-
-import { poseImages } from '../../utils/pose_images'
-
-import './DropDown.css'
+import React from 'react';
+import { poseImages } from '../../utils/pose_images';
+import './DropDown.css';
 
 export default function DropDown({ poseList, currentPose, setCurrentPose }) {
-return (
-        <div
-        className='dropdown dropdown-container'
-         
-      >
-        <button 
-            className="btn btn-secondary dropdown-toggle"
-            type='button'
-            data-bs-toggle="dropdown"
-            id="pose-dropdown-btn"
-            aria-expanded="false"
-        >{currentPose}
-        </button>
-        <ul class="dropdown-menu dropdown-custom-menu" aria-labelledby="dropdownMenuButton1">
-            {poseList.map((pose) => (
-                <li onClick={() => setCurrentPose(pose)}>
-                    <div class="dropdown-item-container">
-                        <p className="dropdown-item-1">{pose}</p>
-                        <img 
-                            src={poseImages[pose]}
-                            className="dropdown-img"
-                        />
-                        
-                    </div>
-                </li>
-            ))}
-            
-        </ul>
-              
-          
-      </div>
-    )
+    return (
+        <div className='dropdown dropdown-container'>
+            <button 
+                className="btn btn-secondary dropdown-toggle"
+                type='button'
+                data-bs-toggle="dropdown"
+                id="pose-dropdown-btn"
+                aria-expanded="false"
+            >
+                {currentPose}
+            </button>
+            <ul className="dropdown-menu dropdown-custom-menu" aria-labelledby="dropdownMenuButton1">
+                {Array.isArray(poseList) && poseList.length > 0 ? (
+                    poseList.map((pose) => (
+                        <li key={pose} onClick={() => setCurrentPose(pose)}>
+                            <div className="dropdown-item-container">
+                                <p className="dropdown-item-1">{pose}</p>
+                                <img 
+                                    src={poseImages[pose]} 
+                                    alt={pose} // Add alt text for accessibility
+                                    className="dropdown-img"
+                                />
+                            </div>
+                        </li>
+                    ))
+                ) : (
+                    <li>
+                        <p className="dropdown-item-1">No poses available</p>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 }
- 
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+// import Header from '../../components/Header/Header';
+// import Footer from '../../components/Footer/Footer';
 import './Home.css';
 
 export default function Home() {
