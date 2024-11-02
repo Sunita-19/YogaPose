da4ef028197d82333cedf6d6a6170acda66c771d
Sunita-19 <sunitayadav190105@gmail.com>
2024-11-02 14:17:25 +0530

    Footer Changed

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,26 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-
 import './Home.css';
 
 export default function Home() {
     return (
         <div className='home-container'>
-
             <div className="home-main">
                 <div className="text-section">
                     <h2 className="heading">Make yoga part<br /> of your life</h2>
                     <p className="subheading">Sharing the love of yoga to create positive change in the world</p>
                 </div>
-                <div className="btn-section">
+            </div>
+            <div className="btn-section">
                     <Link to='/start'>
                         <button className="btn start-btn">Let's Start</button>
                     </Link>
                 </div>
-            </div>
-
-        
         </div>
     );
 }
