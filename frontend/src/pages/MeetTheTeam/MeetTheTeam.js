ca0890fd728d3bdfa74fb367ca26920d5de4e973
Sunita-19 <sunitayadav190105@gmail.com>
2025-04-03 18:06:21 +0530

    Team Members Details

--- frontend/src/pages/MeetTheTeam/MeetTheTeam.js
+++ frontend/src/pages/MeetTheTeam/MeetTheTeam.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './MeetTheTeam.css'; // Make sure this is properly linked
+import './MeetTheTeam.css';
+import TeamSunita from '../../utils/images/TeamSunita.jpeg';
+import TeamMamta from '../../utils/images/TeamMamta.jpeg';
 
 const MeetTheTeam = () => {
   return (
@@ -7,18 +9,45 @@ const MeetTheTeam = () => {
       <h1>Meet the Team</h1>
       
       <div className="team-member">
-        <img src="path_to_sunita_image.jpg" alt="Sunita Yadav" className="team-photo" />
+        <img 
+          src={TeamSunita} 
+          alt="Sunita Yadav" 
+          className="team-photo wrapped" 
+        />
         <div className="team-info">
           <h2>Sunita Yadav</h2>
-          <p>Sunita is the lead developer of the project and has a passion for yoga and technology...</p>
+          <ul className="team-info-list">
+          <li>Crafts intuitive and visually appealing UI/UX designs.</li>
+            <li>Develops responsive front-end code ensuring cross-device compatibility.</li>
+            <li>Maintains consistency in visual design and branding.</li>
+            <li>Creates graphic assets and optimizes them for the web.</li>
+            <li>Ensures seamless communication between design and development teams.</li>
+            <li>Incorporates feedback to continually enhance the application design.</li>
+            <li>Stays updated with modern design trends for continual innovation.</li>
+
+            
+          </ul>
         </div>
       </div>
 
       <div className="team-member">
-        <img src="path_to_mamta_image.jpg" alt="Mamta Yadav" className="team-photo" />
+        <img 
+          src={TeamMamta} 
+          alt="Mamta Yadav" 
+          className="team-photo" 
+        />
         <div className="team-info">
           <h2>Mamta Yadav</h2>
-          <p>Mamta is a creative designer responsible for the UI/UX design of the Yoga Pose Detection system...</p>
+          <ul className="team-info-list">
+          <li>Develops and maintains robust backend server logic.</li>
+            <li>Designs the overall system architecture for efficient data handling.</li>
+            <li>Integrates advanced yoga pose detection algorithms seamlessly.</li>
+            <li>Optimizes database queries for faster performance.</li>
+            <li>Ensures system security and stability at all times.</li>
+            <li>Coordinates API integration with the frontend team.</li>
+            <li>Monitors health, performance, and scalability of the system.</li>
+            <li>Troubleshoots server issues and implements effective fixes.</li>
+          </ul>
         </div>
       </div>
     </div>
