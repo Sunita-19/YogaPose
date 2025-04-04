ddd0dce5c4a8caec67b6fff9617c57eec4f432af
Sunita-19 <sunitayadav190105@gmail.com>
2025-04-04 05:53:56 +0530

    Working without Profile, Replaced Poses and Reset Password

--- frontend/src/utils/pose_images/index.js
+++ frontend/src/utils/pose_images/index.js
@@ -1,45 +1,47 @@
-import chair from './chair.jpg'
-import cobra from './cobra.jpg'
-import dog from './dog.jpg'
-import tree from './tree.jpg'
-import warrior from './warrior.jpg'
-import traingle from './traingle.jpg'
-import shoulderstand from './shoulderstand.jpg'
-import tadasana from './tadasana.jpg'
+import chair from './chair.jpg';
+import cobra from './cobra.jpg';
+import dog from './dog.jpg';
+import tree from './tree.jpg';
+import warrior from './warrior.jpg';
+import traingle from './traingle.jpg';
+import shoulderstand from './shoulderstand.jpg';
+import tadasana from './tadasana.jpg';
 import virabhadrasanaI from './virabhadrasana_i.jpg';
-import balasana from './balasana.jpg'
-import setuBandhasana from './setu_bandhasanan.jpg'
-import marjaryasanaBitilasana from './marjaryasana_bitilasana.jpg'
-import paschimottanasana from './paschimottanasana.jpg'
+import balasana from './balasana.jpg';
+import setuBandhasana from './setu_bandhasanan.jpg';
+import marjaryasanaBitilasana from './marjaryasana_bitilasana.jpg';
+import paschimottanasana from './paschimottanasana.jpg';
 
 // Newly added poses
-import ardhaChandrasana from './ardha_chandrasana.jpg'
-import bakasana from './bakasana.jpg'
-import navasana from './navasana.jpg'
-import phalakasana from './phalakasana.jpg'
-import parivrttaTrikonasana from './parivrtta_trikonasana.jpg'
-import ekaPadaRajakapotasana from './eka_pada_rajakapotasana.jpg'
-import ustrasana from './ustrasana.jpg'
+import ardhaChandrasana from './ardha_chandrasana.jpg';
+import bakasana from './bakasana.jpg';
+import navasana from './navasana.jpg';
+import phalakasana from './phalakasana.jpg';
+import parivrttaTrikonasana from './parivrtta_trikonasana.jpg';
+import ekaPadaRajakapotasana from './eka_pada_rajakapotasana.jpg';
+import ustrasana from './ustrasana.jpg';
 
-export const poseImages = {
-    Tree: tree,
-    Cobra: cobra,
-    Dog: dog,
-    Warrior: warrior,
-    Chair: chair,
-    Triangle: traingle,
-    Shoulderstand: shoulderstand,
-    Tadasana: tadasana,
-    'Virabhadrasana I': virabhadrasanaI,
-    Balasana: balasana,
-    Paschimottanasana: paschimottanasana,
-    'Setu Bandhasana': setuBandhasana, // note the space here
-    'Marjaryasana-Bitilasana': marjaryasanaBitilasana,
-    'Ardha Chandrasana (Half Moon Pose)': ardhaChandrasana,
-    'Bakasana (Crow Pose)': bakasana,
-    'Navasana (Boat Pose)': navasana,
-    'Phalakasana (Plank Pose)': phalakasana,
-    'Parivrtta Trikonasana (Revolved Triangle Pose)': parivrttaTrikonasana,
-    'Eka Pada Rajakapotasana (Pigeon Pose)': ekaPadaRajakapotasana,
-    'Ustrasana (Camel Pose)': ustrasana
-}
+const poseImages = {
+  Tree: tree,
+  Cobra: cobra,
+  Dog: dog,
+  Warrior: warrior,
+  Chair: chair,
+  Triangle: traingle,
+  Shoulderstand: shoulderstand,
+  Tadasana: tadasana,
+  'Virabhadrasana I': virabhadrasanaI,
+  Balasana: balasana,
+  Paschimottanasana: paschimottanasana,
+  'Setu Bandhasana': setuBandhasana,
+  'Marjaryasana-Bitilasana': marjaryasanaBitilasana,
+  'Ardha Chandrasana (Half Moon Pose)': ardhaChandrasana,
+  'Bakasana (Crow Pose)': bakasana,
+  'Navasana (Boat Pose)': navasana,
+  'Phalakasana (Plank Pose)': phalakasana,
+  'Parivrtta Trikonasana (Revolved Triangle Pose)': parivrttaTrikonasana,
+  'Eka Pada Rajakapotasana (Pigeon Pose)': ekaPadaRajakapotasana,
+  'Ustrasana (Camel Pose)': ustrasana
+};
+
+export default poseImages;
